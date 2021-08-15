19b55dcd4c31b60101d240305851cbde548769fc
Andrey A. Vasenev <avas.the.wise@gmail.com>
2021-08-16 00:21:23 +0700

    Added tests and implementations of tgt and tlt commands.

---
+++ src/ShenzhenIO.Emulator.Implementation/Execution/TgtCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ShenzhenIO.Emulator.Core.Execution;
+using ShenzhenIO.Emulator.Core.IO;
+
+namespace ShenzhenIO.Emulator.Implementation.Execution
+{
+    public class TgtCommand : ICommand
+    {
+        private readonly IReadable _firstValueSource;
+        private readonly IReadable _secondValueSource;
+
+        private int? _firstValue;
+        private int? _secondValue;
+
+        public TgtCommand(IReadable firstValueSource, IReadable secondValueSource)
+        {
+            _firstValueSource = firstValueSource ?? throw new ArgumentNullException(nameof(firstValueSource));
+            _secondValueSource = secondValueSource ?? throw new ArgumentNullException(nameof(secondValueSource));
+
+            _firstValue = null;
+            _secondValue = null;
+        }
+
+        public CommandExecutionResult Execute()
+        {
+            if (_firstValue == null)
+            {
+                if (!_firstValueSource.TryRead(out var firstValue))
+                {
+                    return CommandExecutionResult.Blocked();
+                }
+
+                _firstValue = firstValue;
+            }
+
+            if (_secondValue == null)
+            {
+                if (!_secondValueSource.TryRead(out var secondValue))
+                {
+                    return CommandExecutionResult.Blocked();
+                }
+
+                _secondValue = secondValue;
+            }
+
+            var testResult = _firstValue > _secondValue
+                ? TestResult.Success
+                : TestResult.Failure;
+
+            _firstValue = null;
+            _secondValue = null;
+
+            return CommandExecutionResult.SetTestResult(testResult);
+        }
+    }
+}
---
+++ src/ShenzhenIO.Emulator.Tests/Execution/TltCommandTests.cs
@@ -0,0 +1,90 @@
+﻿using Moq;
+using ShenzhenIO.Emulator.Core.Execution;
+using ShenzhenIO.Emulator.Core.IO;
+using ShenzhenIO.Emulator.Implementation.Execution;
+using ShenzhenIO.Emulator.Tests.Extensions;
+using Xunit;
+
+namespace ShenzhenIO.Emulator.Tests.Execution
+{
+    public class TltCommandTests
+    {
+        [Theory]
+        [InlineData(0, 1, TestResult.Success)]
+        [InlineData(1, 0, TestResult.Failure)]
+        [InlineData(1, 1, TestResult.Failure)]
+        [InlineData(-100, 100, TestResult.Success)]
+        [InlineData(100, -100, TestResult.Failure)]
+        [InlineData(-100, -100, TestResult.Failure)]
+        [InlineData(100, 100, TestResult.Failure)]
+        public void TestCheckingValuesImmediately(int first, int second, TestResult expectedTestResult)
+        {
+            // Arrange
+
+            var firstValueSourceMock = new Mock<IReadable>();
+            firstValueSourceMock.Setup(x => x.TryRead(out first)).Returns(true);
+
+            var secondValueSourceMock = new Mock<IReadable>();
+            secondValueSourceMock.Setup(x => x.TryRead(out second)).Returns(true);
+
+            var firstValueSource = firstValueSourceMock.Object;
+            var secondValueSource = secondValueSourceMock.Object;
+            var command = new TltCommand(firstValueSource, secondValueSource);
+
+            // Act
+            var result = command.Execute();
+
+            // Assert
+            result.ShouldSetTestResult(expectedTestResult, "after command execution");
+        }
+
+        [Fact]
+        public void TestExecutionWithBlocks()
+        {
+            // Arrange
+
+            var firstValueSourceMock = new Mock<IReadable>();
+            var first = 0;
+            firstValueSourceMock.Setup(x => x.TryRead(out first)).Returns(false);
+
+            var secondValueSourceMock = new Mock<IReadable>();
+            var second = 0;
+            secondValueSourceMock.Setup(x => x.TryRead(out second)).Returns(false);
+
+            var firstValueSource = firstValueSourceMock.Object;
+            var secondValueSource = secondValueSourceMock.Object;
+            var command = new TltCommand(firstValueSource, secondValueSource);
+
+            // Series of acts and asserts
+
+            var firstResult = command.Execute();
+            firstResult.ShouldCauseABlock("on first value read", "when both values are locked");
+
+            var secondResult = command.Execute();
+            secondResult.ShouldCauseABlock("on first value read", "when both values are still locked");
+
+            first = 1;
+            firstValueSourceMock.Setup(x => x.TryRead(out first)).Returns(true);
+
+            var thirdResult = command.Execute();
+            thirdResult.ShouldCauseABlock("on second value read", "after unlocking the first value");
+
+            second = 10;
+            secondValueSourceMock.Setup(x => x.TryRead(out second)).Returns(true);
+
+            var fourthResult = command.Execute();
+            fourthResult.ShouldSetTestResult(TestResult.Success, "after unlocking both values");
+
+            first = 100;
+            firstValueSourceMock.Setup(x => x.TryRead(out first)).Returns(true);
+
+            var fifthResult = command.Execute();
+            fifthResult.ShouldSetTestResult(TestResult.Failure, "after changing the first value");
+
+            firstValueSourceMock.Setup(x => x.TryRead(out first)).Returns(false);
+
+            var sixthResult = command.Execute();
+            sixthResult.ShouldCauseABlock("on first value read", "after exhausting the first value");
+        }
+    }
+}
